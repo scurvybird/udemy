bc2747fb2b505bd2d4d289dd73bfd3927fa05d19
scurvy_bird <devinzug@gmail.com>
2026-08-04 01:12:39 +0000

    feat: started the capstone project

---
+++ lessons/hiru_101/src/capstone_project/writing_cards.py
@@ -0,0 +1,11 @@
+#say "The current card list contains '#' words."
+#input "Type a word in English to get the French translation: "
+    #output the French translation of the input word
+#input "Do you want to save this card? (yes/no) "
+    #if/else determines if the input gets added to a list of saved words
+#input "Do you want to translate a new word? (yes/no) "
+    #yes continues the loops, no ends the loop
+#print list of saved words
+#input "save cards to file? (yes/no) "
+    #yes writes a new file containing the list of saved words "Saving cards..."
+#End program
